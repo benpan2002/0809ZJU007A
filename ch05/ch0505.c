f2d81f5e9c61acbfda227feb13192829b66bb339
Pan Qing <b20011232@njupt.edu.cn>
2020-10-03 22:25:53 +0800

    check by bp

--- ch05/ch0505.c
+++ ch05/ch0505.c
@@ -1,8 +1,11 @@
 # include <stdio.h>
 
-int Iscomplete = 0;
-
+/**
+ * 
+*/
 int complete_number(int testnumber){
+  int isComplete = 0;
+  //Definite a variable which is available in every part in a bid to show whether certain number will be printed or not 
   int i = 1;
   int sum = 0;
   for(i = 1 ; i < testnumber ; i++){
@@ -12,9 +15,11 @@ int complete_number(int testnumber){
   }
   if(testnumber == sum){
     printf("%d ",sum);
-    Iscomplete = 1;
+    isComplete = 1;
   }
+  return isComplete;
 }
+//Creat a function in a bid to judge whether a number is a 'complete number'
 
 int main(){
   int start = 1;
@@ -23,16 +28,19 @@ int main(){
   printf("Please input n and m, 1<=n<m<1000 .");
   scanf("%d %d",&start,&end);
   if(start < 1 || start >= end || end >= 1000){
-    return;
+    return -1;
   }
+  //Range judgement
+  int isComplete = 0;
   for(testnumber = start ; testnumber <= end ; testnumber++){
-    complete_number(testnumber);}
-    if(Iscomplete = 1){
-      printf("\b");
-    }else{
-      printf("\n");
-    }
-  
+    isComplete = complete_number(testnumber);
+  }
+  if(isComplete == 1){
+    printf("\b");
+  }else{
+    printf("\n");
+  }
+  //Find every 'complete number'
 
   return 0;
 }
